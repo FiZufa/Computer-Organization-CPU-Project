ade52cfffc526c4688ad4dbc4105126bc543af00
Fitria Zu <fi.zufa0612@example.com>
2023-05-02 21:06:35 +0800

    ...hai

---
+++ COMPORG_PROJECT.srcs/sources_1/new/IFetch.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/05/02 19:54:16
+// Design Name: 
+// Module Name: IFetch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module IFetch(
+
+    );
+endmodule
