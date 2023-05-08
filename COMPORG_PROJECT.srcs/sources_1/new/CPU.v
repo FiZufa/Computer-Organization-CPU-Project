66e04ab927a62454c484e2a10cab183dc27599c4
Fitria Zu <fi.zufa0612@example.com>
2023-05-08 18:16:52 +0800

    cpu update

---
+++ COMPORG_PROJECT.srcs/sources_1/new/CPU.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/08/2023 06:01:28 PM
+// Design Name: 
+// Module Name: CPU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CPU(
+    input fpga_rst,
+    input fpga,clk,
+    input[23:0] switch2N4,
+    output[23:0] led2N4
+
+    );
+endmodule
